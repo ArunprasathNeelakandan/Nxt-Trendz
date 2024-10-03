d89ab731394bc1d2aaaec1fd2bab9065720ffb6e
ArunprasathNeelakandan <arunparasathneelakandan@gmail.com>
2024-10-03 15:15:31 +0530

    add initual files

---
+++ src/Component/Login/index.js
@@ -0,0 +1,115 @@
+import React, { useState } from 'react'
+import './index.css'
+
+const Login = (props) => {
+
+    const [username,setUsername] = useState('')
+    const [password,setPassword] = useState('')
+    const [isError,setIsError] = useState(false)
+    const [errorMsgs,setErrorMsg] = useState('')
+
+    const onChangeUsername = event => {
+        setUsername(event.target.value)
+      }
+    
+    const onChangePassword = event => {
+        setPassword( event.target.value)
+      }
+    
+
+    const onSubmitFailure = errorMsg => {
+        setIsError(true)
+        setErrorMsg(errorMsg)
+      }
+
+    const onSubmitSuccess = () => {
+        const {history} = props
+        history.replace('/')
+      }
+    
+
+    const submitForm =async (event) => {
+        event.preventDefault()
+        const url = 'https://apis.ccbp.in/login'
+        const options = {
+            method : 'POST',
+            body : JSON.stringify({username,password})
+        }
+
+        const response = await fetch(url,options)
+        const data = await response.json()
+        if (response.ok === true){
+            onSubmitSuccess()
+        }else {
+            console.log(data)
+            onSubmitFailure(data.error_msg)
+        }
+
+    }
+   const renderUsernameField = () => {
+    
+        return (
+          <>
+            <label className="input-label" htmlFor="username">
+              USERNAME
+            </label>
+            <input
+              type="text"
+              id="username"
+              className="username-input-field"
+              value={username}
+              onChange={onChangeUsername}
+              placeholder="Username"
+            />
+          </>
+        )
+      }
+
+     const renderPasswordField = () => {
+        return (
+          <>
+            <label className="input-label" htmlFor="password">
+              PASSWORD
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="password-input-field"
+              value={password}
+              onChange={onChangePassword}
+              placeholder="Password"
+            />
+          </>
+        )
+      }
+
+  return (
+    <div className="login-form-container">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-logo-img.png"
+          className="login-website-logo-mobile-img"
+          alt="website logo"
+        />
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-login-img.png"
+          className="login-img"
+          alt="website login"
+        />
+        <form className="form-container" onSubmit={submitForm}>
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-logo-img.png"
+            className="login-website-logo-desktop-img"
+            alt="website logo"
+          />
+          <div className="input-container">{renderUsernameField()}</div>
+          <div className="input-container">{renderPasswordField()}</div>
+          <button type="submit" className="login-button">
+            Login
+          </button>
+          {isError  && <p className="error-message">*{errorMsgs}</p>}
+        </form>
+      </div>
+  )
+}
+
+export default Login
